05de701e4d25620db5af2af3301ee3acaddfd909
Joshua Turner <joshflyer5tp@gmail.com>
2016-11-25 05:22:03 -0500

    initialized led pin 13 as an output and named it "LED",  added if statements to handle "light" command and reply what the state of the light is, added to the command replies in if (c == "go"), removed old comments, commented out the "light not implemented else if.

--- Arduino/UNO_R3_Main/UNO_R3_Main.ino
+++ Arduino/UNO_R3_Main/UNO_R3_Main.ino
@@ -12,99 +12,113 @@
 //
 // end license header
 //
-// This sketch is a good place to start if you're just getting started with 
-// Pixy and Arduino.  This program simply prints the detected object blocks 
-// (including color codes) through the serial console.  It uses the Arduino's 
+// This sketch is a good place to start if you're just getting started with
+// Pixy and Arduino.  This program simply prints the detected object blocks
+// (including color codes) through the serial console.  It uses the Arduino's
 // ICSP port.  For more information go here:
 //
 // http://cmucam.org/projects/cmucam5/wiki/Hooking_up_Pixy_to_a_Microcontroller_(like_an_Arduino)
 //
 
-   
-#include <SPI.h>  
+
+#include <SPI.h>
 #include <Pixy.h>
 
-// This is the main Pixy object 
+int LED = 13;
+// This is the main Pixy object
 Pixy pixy;
 
 void setup()
 {
-  pinMode(13, OUTPUT);
+  pinMode(LED, OUTPUT);
   Serial.begin(57600);
   pixy.init();
   Serial.print("on\n");
   delay(1000);
   Serial.print("waiting\n");
+  String data = "wait";
 
-  
 }
 
 void loop()
-{ 
+{
   static String C = "wait";
-  static String data = "wait";
+  String data;
   int j;
   uint16_t blocks;
-  char buf[32]; 
-  
+  char buf[32];
+
   // grab blocks!
   blocks = pixy.getBlocks();
-  
-  
+
+
   if (Serial.available() > 0)
   {
-    C = Serial.readString();
+    data = Serial.readString();
+    if (data == "light")
+    {
+      if (C != "go" && LED == HIGH)
+      {
+        digitalWrite(LED, LOW);
+        Serial.print("light off\n"); //command reply string
+        delay(500);
+      }
+      else if (C != "go" && LED == LOW)
+      {
+        digitalWrite(LED, HIGH);
+        Serial.print("light on\n"); //command reply string
+        delay(500);
+      }
+    }
+    else
+    {
+      C == data;
+    }
     if (C == "go")
     {
-      Serial.print("User command: " + C + "\n");
-      //delay(500);
-      //Serial.print("Data start in 1 second!\n");
+      digitalWrite(LED, HIGH);
+      Serial.print("User command: go\nlight on\n"); //command reply string
       delay(1000);
     }
     else
     {
-    Serial.print("User command: " + C + "\n");
+      digitalWrite(LED, LOW);
+      Serial.print("User command: " + C + "\n" + "light off\n"); //command reply string
     }
   }
-// else
-//{
 
-//} 
-  if(C == "go")
+  if (C == "go")
   {
-     if (blocks)// If there are blocks detected, print them!
-     {
-                  //i++;
-                   // do this (print) every 2 frames because printing every
-                   // frame would bog down the Arduino(not)
-                   /*if (i%2==0)
-                     {*/
-       for (j=0; j<blocks; j++)
-       {
-       //sprintf(buf, "  block %d: ", j);
-       //Serial.print(buf); 
-       pixy.blocks[j].print();
-       }
-         //}
-      }  
+    if (blocks)// If there are blocks detected, print them!
+    {
+      //i++;
+      // do this (print) every 2 frames because printing every
+      // frame would bog down the Arduino(not)
+      /*if (i%2==0)
+        {*/
+      for (j = 0; j < blocks; j++)
+      {
+        //sprintf(buf, "  block %d: ", j);
+        //Serial.print(buf);
+        pixy.blocks[j].print();
+      }
+      //}
+    }
   }
-/*  else if(C == "stop")
+
+  else if (C == "wait")
   {
-    C = "wait";
-  } */
-  else if(C == "wait")
-  {
-    //C = "hold";
+
     delay(250);
   }
-  else if(C == "light")
-  {
-    Serial.print("not implemented\n");
-    C = "wait";
-    Serial.print("waiting\n");
-    delay(250);
-  }
-  else if(C == "help")
+  /* else if(C == "light")
+   {
+     Serial.print("not implemented\n");
+     C = "wait";
+     Serial.print("waiting\n");
+     delay(250);
+   }  */
+  else if (C == "help")
   {
     Serial.print("command list:\ncommand: go = activate camera & start data stream\n");
     Serial.print("command: wait = stop data stream\n");
